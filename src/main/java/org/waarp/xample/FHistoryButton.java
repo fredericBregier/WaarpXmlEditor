cf59933656fc1dc224eaa9018b29a7c54ff316b7
Frederic Bregier <frederic.bregier@waarp.fr>
2019-07-12 11:25:06 +0200

    Fix pom

--- src/main/java/org/waarp/xample/FHistoryButton.java
+++ src/main/java/org/waarp/xample/FHistoryButton.java
@@ -4,14 +4,32 @@ package org.waarp.xample;
  * Copyright (c) 2002 Felix Golubov
  */
 
-import java.util.*;
-import java.util.List;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import javax.swing.event.*;
 import com.fg.util.FLoader;
 import com.fg.util.FadingFilter;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import javax.swing.JToggleButton;
+import javax.swing.SwingUtilities;
+import javax.swing.event.PopupMenuEvent;
+import javax.swing.event.PopupMenuListener;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Insets;
+import java.awt.ItemSelectable;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple button with drop-down menu for the XAmple application.
@@ -21,197 +39,180 @@ import com.fg.util.FadingFilter;
  */
 
 public class FHistoryButton extends JComponent
-  implements ItemSelectable, ActionListener
-{
-  class Btn extends JToggleButton
-    implements MouseListener, PopupMenuListener
-  {
-    boolean b = false;
-
-    public Btn()
-    {
-      super();
-      setFocusPainted(false);
-      this.addMouseListener(this);
+        implements ItemSelectable, ActionListener {
+    JToggleButton leftButton = new JToggleButton();
+    Btn rightButton = new Btn();
+    JPopupMenu popupMenu = new JPopupMenu();
+    ArrayList items = new ArrayList();
+    ArrayList itemListeners = new ArrayList(1);
+    ArrayList actionListeners = new ArrayList(1);
+    public FHistoryButton(ImageIcon icon, String leftTipText, String rightTipText) {
+        super();
+        leftButton.setFocusPainted(false);
+        setIcon(icon);
+        ImageIcon icoArrow = FLoader.getIcon(this, "DropDown.gif");
+        rightButton.setIcon(icoArrow);
+        rightButton.setDisabledIcon(FadingFilter.fade(icoArrow));
+        leftButton.setToolTipText(leftTipText);
+        rightButton.setToolTipText(rightTipText);
+        popupMenu.addPopupMenuListener(rightButton);
+        leftButton.addActionListener(this);
+        setBorder(BorderFactory.createLineBorder(Color.gray));
+        setLayout(new BorderLayout());
+        add(leftButton, BorderLayout.CENTER);
+        add(rightButton, BorderLayout.EAST);
+        setPreferredSize(new Dimension(44 + 14, 27));
+        setMinimumSize(new Dimension(44 + 14, 27));
+        setMaximumSize(new Dimension(44 + 14, 27));
+        setItems(null);
     }
 
-    public Dimension getMinimumSize()
-    {
-      Dimension d = super.getMinimumSize();
-      return new Dimension(14, 27);
+    public Insets getInsets() {
+        return new Insets(2, 2, 1, 1);
     }
 
-    public Dimension getPreferredSize()
-    {
-      Dimension d = super.getPreferredSize();
-      return new Dimension(14, 27);
-    }
-
-    public void mouseClicked(MouseEvent e)
-    {
-    }
-
-    public void mousePressed(MouseEvent e)
-    {
-      if (!isEnabled())
-        return;
-      if (popupMenu.isShowing())
-        b = true;
-      else
-        popupMenu.show(FHistoryButton.this, 0, FHistoryButton.this.getSize().height);
-    }
-
-    public void mouseReleased(MouseEvent e) {}
-    public void mouseEntered(MouseEvent e) {}
-    public void mouseExited(MouseEvent e) {}
-    public void popupMenuCanceled(PopupMenuEvent e) {}
-
-    public void popupMenuWillBecomeInvisible(PopupMenuEvent e)
-    {
-      if (b)
-        b = false;
-      else
-        setSelected(false);
-    }
-
-    public void popupMenuWillBecomeVisible(PopupMenuEvent e)
-    {
-    }
-  }
-
-  JToggleButton leftButton = new JToggleButton();
-  Btn rightButton = new Btn();
-  JPopupMenu popupMenu = new JPopupMenu();
-  ArrayList items = new ArrayList();
-  ArrayList itemListeners = new ArrayList(1);
-  ArrayList actionListeners = new ArrayList(1);
-
-  public FHistoryButton(ImageIcon icon, String leftTipText, String rightTipText)
-  {
-    super();
-    leftButton.setFocusPainted(false);
-    setIcon(icon);
-    ImageIcon icoArrow = FLoader.getIcon(this, "DropDown.gif");
-    rightButton.setIcon(icoArrow);
-    rightButton.setDisabledIcon(FadingFilter.fade(icoArrow));
-    leftButton.setToolTipText(leftTipText);
-    rightButton.setToolTipText(rightTipText);
-    popupMenu.addPopupMenuListener(rightButton);
-    leftButton.addActionListener(this);
-    setBorder(BorderFactory.createLineBorder(Color.gray));
-    setLayout(new BorderLayout());
-    add(leftButton, BorderLayout.CENTER);
-    add(rightButton, BorderLayout.EAST);
-    setPreferredSize(new Dimension(44 + 14, 27));
-    setMinimumSize(new Dimension(44 + 14, 27));
-    setMaximumSize(new Dimension(44 + 14, 27));
-    setItems(null);
-  }
-
-  public Insets getInsets()
-  {
-    return new Insets(2, 2, 1, 1);
-  }
-
-  public void setIcon(ImageIcon icon)
-  {
-    leftButton.setIcon(icon);
-    if (icon != null)
-      leftButton.setDisabledIcon(FadingFilter.fade(icon));
-  }
-
-  public void setItems(List items)
-  {
-    popupMenu.removeAll();
-    this.items.clear();
-    if (items != null)
-    {
-      for (int i = 0; i < items.size(); i++)
-      {
-        Object item = items.get(i);
-        if (item == null)
-          continue;
-        this.items.add(item);
-        JMenuItem mi = new JMenuItem(item.toString());
-        mi.addActionListener(this);
-        popupMenu.add(mi);
-      }
-    }
-    if (this.items.size() == 0)
-    {
-      JMenuItem mi = new JMenuItem("< Empty >");
-      mi.setEnabled(false);
-      popupMenu.add(mi);
-      return;
-    }
-  }
-
-  public void actionPerformed(ActionEvent e)
-  {
-    if (e.getSource() instanceof JMenuItem)
-    {
-      int index = popupMenu.getComponentIndex((Component)e.getSource());
-      if (index >= 0 && index < items.size())
-      {
-        ItemEvent ie = new ItemEvent(this, index, items.get(index),
-          ItemEvent.SELECTED);
-        for (int i = 0; i < itemListeners.size(); i++)
-        {
-          ItemListener l = (ItemListener)itemListeners.get(i);
-          l.itemStateChanged(ie);
+    public void setIcon(ImageIcon icon) {
+        leftButton.setIcon(icon);
+        if (icon != null) {
+            leftButton.setDisabledIcon(FadingFilter.fade(icon));
         }
-      }
     }
-    else if (e.getSource() == leftButton)
-    {
-      leftButton.setSelected(false);
 
-      for (int i = 0; i < actionListeners.size(); i++)
-      {
-        ActionListener l = (ActionListener)actionListeners.get(i);
-        ActionEvent ae = new ActionEvent(this, e.getID(), e.getActionCommand());
-        l.actionPerformed(ae);
-      }
+    public void setItems(List items) {
+        popupMenu.removeAll();
+        this.items.clear();
+        if (items != null) {
+            for (int i = 0; i < items.size(); i++) {
+                Object item = items.get(i);
+                if (item == null) {
+                    continue;
+                }
+                this.items.add(item);
+                JMenuItem mi = new JMenuItem(item.toString());
+                mi.addActionListener(this);
+                popupMenu.add(mi);
+            }
+        }
+        if (this.items.size() == 0) {
+            JMenuItem mi = new JMenuItem("< Empty >");
+            mi.setEnabled(false);
+            popupMenu.add(mi);
+            return;
+        }
     }
-  }
 
-  public void addItemListener(ItemListener l)
-  {
-    if (!itemListeners.contains(l))
-      itemListeners.add(l);
-  }
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() instanceof JMenuItem) {
+            int index = popupMenu.getComponentIndex((Component) e.getSource());
+            if (index >= 0 && index < items.size()) {
+                ItemEvent ie = new ItemEvent(this, index, items.get(index),
+                                             ItemEvent.SELECTED);
+                for (int i = 0; i < itemListeners.size(); i++) {
+                    ItemListener l = (ItemListener) itemListeners.get(i);
+                    l.itemStateChanged(ie);
+                }
+            }
+        } else if (e.getSource() == leftButton) {
+            leftButton.setSelected(false);
 
-  public Object[] getSelectedObjects()
-  {
-    return null;
-  }
+            for (int i = 0; i < actionListeners.size(); i++) {
+                ActionListener l = (ActionListener) actionListeners.get(i);
+                ActionEvent ae = new ActionEvent(this, e.getID(), e.getActionCommand());
+                l.actionPerformed(ae);
+            }
+        }
+    }
 
-  public void removeItemListener(ItemListener l)
-  {
-    itemListeners.remove(l);
-  }
+    public void addItemListener(ItemListener l) {
+        if (!itemListeners.contains(l)) {
+            itemListeners.add(l);
+        }
+    }
 
-  public void addActionListener(ActionListener l)
-  {
-    if (!actionListeners.contains(l))
-      actionListeners.add(l);
-  }
+    public Object[] getSelectedObjects() {
+        return null;
+    }
 
-  public void removeActionListener(ActionListener l)
-  {
-    actionListeners.remove(l);
-  }
+    public void removeItemListener(ItemListener l) {
+        itemListeners.remove(l);
+    }
 
-  public void setEnabled(boolean enabled)
-  {
-    super.setEnabled(enabled);
-    leftButton.setEnabled(enabled);
-    rightButton.setEnabled(enabled);
-  }
+    public void addActionListener(ActionListener l) {
+        if (!actionListeners.contains(l)) {
+            actionListeners.add(l);
+        }
+    }
 
-  public void updateUI()
-  {
-    super.updateUI();
-    SwingUtilities.updateComponentTreeUI(popupMenu);
-  }
+    public void removeActionListener(ActionListener l) {
+        actionListeners.remove(l);
+    }
+
+    public void setEnabled(boolean enabled) {
+        super.setEnabled(enabled);
+        leftButton.setEnabled(enabled);
+        rightButton.setEnabled(enabled);
+    }
+
+    public void updateUI() {
+        super.updateUI();
+        SwingUtilities.updateComponentTreeUI(popupMenu);
+    }
+
+    class Btn extends JToggleButton
+            implements MouseListener, PopupMenuListener {
+        boolean b = false;
+
+        public Btn() {
+            super();
+            setFocusPainted(false);
+            this.addMouseListener(this);
+        }
+
+        public Dimension getMinimumSize() {
+            Dimension d = super.getMinimumSize();
+            return new Dimension(14, 27);
+        }
+
+        public Dimension getPreferredSize() {
+            Dimension d = super.getPreferredSize();
+            return new Dimension(14, 27);
+        }
+
+        public void mouseClicked(MouseEvent e) {
+        }
+
+        public void mousePressed(MouseEvent e) {
+            if (!isEnabled()) {
+                return;
+            }
+            if (popupMenu.isShowing()) {
+                b = true;
+            } else {
+                popupMenu.show(FHistoryButton.this, 0, FHistoryButton.this.getSize().height);
+            }
+        }
+
+        public void mouseReleased(MouseEvent e) {
+        }
+
+        public void mouseEntered(MouseEvent e) {
+        }
+
+        public void mouseExited(MouseEvent e) {
+        }
+
+        public void popupMenuCanceled(PopupMenuEvent e) {
+        }
+
+        public void popupMenuWillBecomeInvisible(PopupMenuEvent e) {
+            if (b) {
+                b = false;
+            } else {
+                setSelected(false);
+            }
+        }
+
+        public void popupMenuWillBecomeVisible(PopupMenuEvent e) {
+        }
+    }
 }
--- src/main/java/org/waarp/xample/custnodes/FFileDialog.java
+++ src/main/java/org/waarp/xample/custnodes/FFileDialog.java
@@ -1,17 +1,16 @@
 /**
  * This file is part of Waarp Project.
- * 
- * Copyright 2009, Frederic Bregier, and individual contributors by the @author tags. See the
- * COPYRIGHT.txt in the distribution for a full listing of individual contributors.
- * 
- * All Waarp Project is free software: you can redistribute it and/or modify it under the terms of
- * the GNU General Public License as published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
- * 
- * Waarp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details.
- * 
+ * <p>
+ * Copyright 2009, Frederic Bregier, and individual contributors by the @author tags. See the COPYRIGHT.txt in the
+ * distribution for a full listing of individual contributors.
+ * <p>
+ * All Waarp Project is free software: you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * <p>
+ * Waarp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * <p>
  * You should have received a copy of the GNU General Public License along with Waarp . If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -21,24 +20,33 @@ package org.waarp.xample.custnodes;
  * Copyright (c) 2003 Felix Golubov
  */
 
-import java.util.*;
-import java.util.List;
-import java.io.*;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import com.fg.ftreenodes.Params;
 import com.fg.ftreenodes.ICellControl;
+import com.fg.ftreenodes.Params;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.util.HashSet;
+import java.util.List;
+import java.util.StringTokenizer;
 
 /**
  * FFileDialog is a file dialog, which can be used as a custom field editor for the
  * {@link com.fg.xmleditor.FXBasicView}. Since FFileDialog is a JDialog, it can be used as both
  * custom field editor and a global editor.
- * 
+ *
  * @author Felix Golubov
  * @author frederic Bregier
  * @version 2.0
- * 
+ *
  *          Add support for Directory and different options on File Dialog
  */
 
@@ -51,198 +59,182 @@ import com.fg.ftreenodes.ICellControl;
  */
 
 public class FFileDialog extends JDialog
-		implements ICellControl, ActionListener
-{
-	JPanel panel = new JPanel();
-	FFileChooser fileChooser = new FFileChooser();
-	int mode = JFileChooser.FILES_ONLY;
-	Object data;
-	List masks = null;
+        implements ICellControl, ActionListener {
+    JPanel panel = new JPanel();
+    FFileChooser fileChooser = new FFileChooser();
+    int mode = JFileChooser.FILES_ONLY;
+    Object data;
+    List masks = null;
 
-	class FileExtFilter extends javax.swing.filechooser.FileFilter
-	{
-		HashSet<String> extensions = new HashSet<String>();
-		String description = "";
+    public FFileDialog() {
+        this.getContentPane().setLayout(new BorderLayout());
+        fileChooser.setCurrentDirectory(new File("."));
+        fileChooser.addActionListener(this);
+        fileChooser.setApproveButtonText("OK");
+        this.getContentPane().add(fileChooser, BorderLayout.CENTER);
+    }
 
-		public FileExtFilter(String mask)
-		{
-			StringTokenizer st = new StringTokenizer(mask, "|");
-			while (st.hasMoreElements())
-			{
-				String token = st.nextToken().trim();
-				if (st.hasMoreElements()) {
-					if (token.length() == 0) {
-						// no extension
-					} else {
-						if (token.startsWith("null")) {
-							if (token.length() > 4) {
-								token = token.substring(5).trim();
-							} else {
-								token = "";
-							}
-						}
-						if (token.length() > 0) {
-							extensions.add(token.toLowerCase());
-						}
-					}
-				} else {
-					String type = token.substring(0, 1);
-					int val = Integer.parseInt(type);
-					if (val == 0) {
-						mode = JFileChooser.FILES_ONLY;
-					} else if (val == 1) {
-						mode = JFileChooser.DIRECTORIES_ONLY;
-					} else {
-						mode = JFileChooser.FILES_AND_DIRECTORIES;
-					}
-					description = token.substring(1);
-				}
-			}
-			fileChooser.setFileSelectionMode(mode);
-		}
+    protected void processWindowEvent(WindowEvent e) {
+        if (e.getID() == WindowEvent.WINDOW_CLOSING) {
+            cancel();
+        }
+        super.processWindowEvent(e);
+    }
 
-		public boolean accept(File f)
-		{
-			if (f.isDirectory())
-				return true;
-			if (f.isFile() && mode == JFileChooser.DIRECTORIES_ONLY)
-				return false;
-			if (extensions.size() == 0)
-				return true;
-			String ext = null;
-			String name = f.getName();
-			int i = name.lastIndexOf('.');
-			if (i > 0 && i < name.length() - 1)
-			{
-				ext = name.substring(i + 1).toLowerCase();
-				return extensions.contains(ext);
-			}
-			else
-				return false;
-		}
+    void cancel() {
+        dispose();
+    }
 
-		public String getDescription()
-		{
-			return description;
-		}
-	}
+    public void actionPerformed(ActionEvent e) {
+        if (!(e.getSource() instanceof JButton)) {
+            return;
+        }
+        JButton btn = (JButton) e.getSource();
+        if ("OK".equals(btn.getText())) {
+            File file = fileChooser.getSelectedFile();
+            // data = (file == null || file.isDirectory()) ? "" : file.getAbsolutePath();
+            data = (file == null)? "" : file.getAbsolutePath();
+            cancel();
+        } else if ("Cancel".equals(btn.getText())) {
+            cancel();
+        } else if ("Annuler".equals(btn.getText())) {
+            cancel();
+        }
+    }
 
-	class FFileChooser extends JFileChooser
-	{
-		public void addBtnListener(Component c)
-		{
-			if (c instanceof JButton)
-				((JButton) c).addActionListener(FFileDialog.this);
-			if (c instanceof Container)
-			{
-				Component[] children = ((Container) c).getComponents();
-				for (int i = 0; i < children.length; i++)
-					addBtnListener(children[i]);
-			}
-		}
+    public void initCellControl(boolean isEditor) {
+        fileChooser.addBtnListener(fileChooser);
+    }
 
-		public void updateUI()
-		{
-			setAcceptAllFileFilterUsed(getAcceptAllFileFilter() == null);
-			super.updateUI();
-			setAcceptAllFileFilterUsed(true);
-			addBtnListener(this);
-		}
-	}
+    public void updateCellControl(boolean isEditor, boolean enabled,
+                                  boolean editable, Object data, Params params) {
+        pack();
+        this.data = data;
+        if (data != null) {
+            String path = data.toString().trim();
+            if (path.length() > 0) {
+                try {
+                    File file = new File(path);
+                    fileChooser.setSelectedFile(file);
+                } catch (Exception ex) {
+                    fileChooser.setSelectedFile(null);
+                }
+            } else {
+                fileChooser.setSelectedFile(null);
+            }
+        } else {
+            fileChooser.setSelectedFile(null);
+        }
+        List list = params.getList();
+        boolean sameMasks = true;
+        if (masks == null || list.size() != masks.size()) {
+            sameMasks = false;
+        } else {
+            for (int i = 0; i < list.size() && sameMasks; i++) {
+                if (!list.get(i).equals(masks.get(i))) {
+                    sameMasks = false;
+                }
+            }
+        }
+        if (sameMasks) {
+            return;
+        }
+        masks = list;
+        fileChooser.resetChoosableFileFilters();
+        for (int i = 0; i < masks.size(); i++) {
+            fileChooser.addChoosableFileFilter(
+                    new FileExtFilter((String) masks.get(i)));
+        }
+    }
 
-	public FFileDialog()
-	{
-		this.getContentPane().setLayout(new BorderLayout());
-		fileChooser.setCurrentDirectory(new File("."));
-		fileChooser.addActionListener(this);
-		fileChooser.setApproveButtonText("OK");
-		this.getContentPane().add(fileChooser, BorderLayout.CENTER);
-	}
+    public Object getData() {
+        return data;
+    }
 
-	protected void processWindowEvent(WindowEvent e)
-	{
-		if (e.getID() == WindowEvent.WINDOW_CLOSING) {
-			cancel();
-		}
-		super.processWindowEvent(e);
-	}
+    class FileExtFilter extends javax.swing.filechooser.FileFilter {
+        HashSet<String> extensions = new HashSet<String>();
+        String description = "";
 
-	void cancel()
-	{
-		dispose();
-	}
+        public FileExtFilter(String mask) {
+            StringTokenizer st = new StringTokenizer(mask, "|");
+            while (st.hasMoreElements()) {
+                String token = st.nextToken().trim();
+                if (st.hasMoreElements()) {
+                    if (token.length() == 0) {
+                        // no extension
+                    } else {
+                        if (token.startsWith("null")) {
+                            if (token.length() > 4) {
+                                token = token.substring(5).trim();
+                            } else {
+                                token = "";
+                            }
+                        }
+                        if (token.length() > 0) {
+                            extensions.add(token.toLowerCase());
+                        }
+                    }
+                } else {
+                    String type = token.substring(0, 1);
+                    int val = Integer.parseInt(type);
+                    if (val == 0) {
+                        mode = JFileChooser.FILES_ONLY;
+                    } else if (val == 1) {
+                        mode = JFileChooser.DIRECTORIES_ONLY;
+                    } else {
+                        mode = JFileChooser.FILES_AND_DIRECTORIES;
+                    }
+                    description = token.substring(1);
+                }
+            }
+            fileChooser.setFileSelectionMode(mode);
+        }
 
-	public void actionPerformed(ActionEvent e)
-	{
-		if (!(e.getSource() instanceof JButton))
-			return;
-		JButton btn = (JButton) e.getSource();
-		if ("OK".equals(btn.getText()))
-		{
-			File file = fileChooser.getSelectedFile();
-			// data = (file == null || file.isDirectory()) ? "" : file.getAbsolutePath();
-			data = (file == null) ? "" : file.getAbsolutePath();
-			cancel();
-		}
-		else if ("Cancel".equals(btn.getText()))
-			cancel();
-		else if ("Annuler".equals(btn.getText()))
-			cancel();
-	}
+        public boolean accept(File f) {
+            if (f.isDirectory()) {
+                return true;
+            }
+            if (f.isFile() && mode == JFileChooser.DIRECTORIES_ONLY) {
+                return false;
+            }
+            if (extensions.size() == 0) {
+                return true;
+            }
+            String ext = null;
+            String name = f.getName();
+            int i = name.lastIndexOf('.');
+            if (i > 0 && i < name.length() - 1) {
+                ext = name.substring(i + 1).toLowerCase();
+                return extensions.contains(ext);
+            } else {
+                return false;
+            }
+        }
 
-	public void initCellControl(boolean isEditor)
-	{
-		fileChooser.addBtnListener(fileChooser);
-	}
+        public String getDescription() {
+            return description;
+        }
+    }
 
-	public void updateCellControl(boolean isEditor, boolean enabled,
-			boolean editable, Object data, Params params)
-	{
-		pack();
-		this.data = data;
-		if (data != null)
-		{
-			String path = data.toString().trim();
-			if (path.length() > 0)
-			{
-				try {
-					File file = new File(path);
-					fileChooser.setSelectedFile(file);
-				} catch (Exception ex) {
-					fileChooser.setSelectedFile(null);
-				}
-			}
-			else
-				fileChooser.setSelectedFile(null);
-		}
-		else
-			fileChooser.setSelectedFile(null);
-		List list = params.getList();
-		boolean sameMasks = true;
-		if (masks == null || list.size() != masks.size())
-			sameMasks = false;
-		else
-		{
-			for (int i = 0; i < list.size() && sameMasks; i++)
-			{
-				if (!list.get(i).equals(masks.get(i)))
-					sameMasks = false;
-			}
-		}
-		if (sameMasks)
-			return;
-		masks = list;
-		fileChooser.resetChoosableFileFilters();
-		for (int i = 0; i < masks.size(); i++)
-		{
-			fileChooser.addChoosableFileFilter(
-					new FileExtFilter((String) masks.get(i)));
-		}
-	}
+    class FFileChooser extends JFileChooser {
+        public void addBtnListener(Component c) {
+            if (c instanceof JButton) {
+                ((JButton) c).addActionListener(FFileDialog.this);
+            }
+            if (c instanceof Container) {
+                Component[] children = ((Container) c).getComponents();
+                for (int i = 0; i < children.length; i++) {
+                    addBtnListener(children[i]);
+                }
+            }
+        }
 
-	public Object getData()
-	{
-		return data;
-	}
+        public void updateUI() {
+            setAcceptAllFileFilterUsed(getAcceptAllFileFilter() == null);
+            super.updateUI();
+            setAcceptAllFileFilterUsed(true);
+            addBtnListener(this);
+        }
+    }
 
 }
